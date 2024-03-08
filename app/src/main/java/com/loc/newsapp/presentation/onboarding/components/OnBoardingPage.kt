5c8e6ccab68ad43f45baabb3f158e9c67da7e93d
Chamika <chamikachathushka8@gmail.com>
2024-03-08 19:17:05 +0530

    first commit

---
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/components/OnBoardingPage.kt
@@ -0,0 +1,61 @@
+package com.loc.newsapp.presentation.onboarding.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.loc.newsapp.R
+import com.loc.newsapp.presentation.Dimensions.MediumPadding1
+import com.loc.newsapp.presentation.Dimensions.MediumPadding2
+import com.loc.newsapp.presentation.onboarding.Page
+import com.loc.newsapp.presentation.onboarding.pages
+import com.loc.newsapp.ui.theme.NewsAppTheme
+
+@Composable
+fun OnBoardingPage(
+    modifier: Modifier = Modifier,
+    page: Page
+) {
+    Column(modifier = modifier) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(fraction = 0.6f),
+            painter = painterResource(id = page.image),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.height(MediumPadding1))
+
+        Text(
+            text = page.title,
+            modifier = Modifier.padding(horizontal = MediumPadding2),
+            style = MaterialTheme.typography.displaySmall.copy(fontWeight = FontWeight.Bold),
+            color = colorResource(id = R.color.display_small)
+        )
+        Text(
+            text = page.description,
+            modifier = Modifier.padding(horizontal = MediumPadding2),
+            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
+            color = colorResource(id = R.color.text_medium)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun onBoardingPagePreview(){
+    NewsAppTheme {
+        OnBoardingPage(page = pages[0])
+    }
+}
